19a34a69d51c17c450185cfeb94c690cfb6567cc
Rodrigo Schwartz <rodrigoalexschwartz@gmail.com>
2019-10-22 17:03:42 -0300

    vaaaai

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -7,6 +7,34 @@
         </material-card>
       </v-col>
     </v-row>
+        <v-dialog v-model="dialog" persistent max-width="600px">
+      <template v-slot:activator="{ on }">
+        <v-btn color="orange" fab v-on="on"><v-icon>mdi-plus</v-icon></v-btn>
+      </template>
+      <v-card>
+        <v-card-title>
+          <span class="headline">Projeto</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field label="Horas*" required></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6">
+                <v-autocomplete :items="this.projects" label="Projeto*"></v-autocomplete>
+              </v-col>
+            </v-row>
+          </v-container>
+          <small>*indicates required field</small>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="red darken-1" text @click="dialog = false">Close</v-btn>
+          <v-btn color="orange darken-1" text @click="dialog = false">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
   
@@ -56,6 +84,7 @@ export default {
 
   data() {
     return {
+      dialog: false,
       search: "",
       projects: [],
       header: [
--- src/views/Value.vue
+++ src/views/Value.vue
@@ -7,6 +7,34 @@
         </material-card>
       </v-col>
     </v-row>
+        <v-dialog v-model="dialog" persistent max-width="600px">
+      <template v-slot:activator="{ on }">
+        <v-btn color="green" fab v-on="on"><v-icon>mdi-plus</v-icon></v-btn>
+      </template>
+      <v-card>
+        <v-card-title>
+          <span class="headline">Apontamento de Gastos</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field label="Gastos *" required></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6">
+                <v-autocomplete :items="this.projects" label="Projeto*"></v-autocomplete>
+              </v-col>
+            </v-row>
+          </v-container>
+          <small>*indicates required field</small>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="red darken-1" text @click="dialog = false">Close</v-btn>
+          <v-btn color="green darken-1" text @click="dialog = false">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
 
@@ -49,6 +77,7 @@ export default {
 
   data() {
     return {
+      dialog: false,
       search: "",
       values: [],
       headers: [
