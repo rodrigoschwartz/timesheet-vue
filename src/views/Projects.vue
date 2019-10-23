3d287778d4561c7cb8270e4ecedcba2af2ed6dce
Rodrigo Schwartz <rodrigoalexschwartz@gmail.com>
2019-10-23 16:21:00 -0300

    foi

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,81 +1,119 @@
 <template>
   <v-container fluid>
-    <v-row justify="left">
+    <v-row justify="center">
       <v-col cols="10">
         <material-card color="orange" title="Projetos" text>
           <v-data-table :headers="header" :items="projects" :search="search" hide-default-footer />
         </material-card>
       </v-col>
     </v-row>
-    <v-dialog v-model="dialog" persistent max-width="600px">
-      <template v-slot:activator="{ on }">
-        <v-btn color="orange" fab v-on="on">
-          <v-icon>mdi-plus</v-icon>
-        </v-btn>
-      </template>
-      <v-card>
-        <v-card-title>
-          <span class="headline">Projeto</span>
-        </v-card-title>
-        <v-card-text>
-          <v-container>
-            <v-row>
-              <v-col cols="12" sm="6" md="4">
-                <v-text-field v-model="demand" label="Demanda*" required></v-text-field>
-              </v-col>
-              <v-col cols="30" sm="10" md="8">
-                <v-text-field v-model="description" label="Descrição*" required></v-text-field>
-              </v-col>
-              <v-col cols="12" sm="6" md="6">
-                <v-text-field v-model="value" label="Orçamento*" required></v-text-field>
-              </v-col>
-              <v-col cols="12" sm="6" md="6">
-                <v-text-field v-model="hour" label="Horas Disponíveis*" required></v-text-field>
-              </v-col>
-              <v-col cols="12" sm="6">
-                <v-select v-model="status_field" :items="this.status" label="Status*"></v-select>
-              </v-col>
-              <v-col cols="12" sm="6">
-                <v-select v-model="user_field" :items="this.users" label="Responsável*"></v-select>
-              </v-col>
-            </v-row>
-          </v-container>
-          <small>*indicates required field</small>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="red darken-1" text @click="dialog = false">Close</v-btn>
-          <v-btn color="orange darken-1" text @click="saveData">Save</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-    <v-dialog v-model="delete_dialog" persistent max-width="600px">
-      <template v-slot:activator="{ on }">
-        <v-btn color="orange" fab v-on="on">
-          <v-icon>mdi-plus</v-icon>
-        </v-btn>
-      </template>
-      <v-card>
-        <v-card-title>
-          <span class="headline">Eliminar Projeto</span>
-        </v-card-title>
-        <v-card-text>
-          <v-container>
-            <v-row>
-              <v-col cols="12" sm="6">
-                <v-select v-model="project_field" :items="this.projetos" label="Projetos*"></v-select>
-              </v-col>
-            </v-row>
-          </v-container>
-          <small>*indicates required field</small>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="red darken-1" text @click="delete_dialog = false">Close</v-btn>
-          <v-btn color="orange darken-1" text @click="deleteData">Delete</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+    <v-row justify="center">
+      <v-col cols="1">
+        <v-dialog v-model="dialog" persistent max-width="600px">
+          <template v-slot:activator="{ on }">
+            <v-btn color="orange" v-on="on">
+              <v-icon>mdi-plus</v-icon>
+            </v-btn>
+          </template>
+          <v-card>
+            <v-card-title>
+              <span class="headline">Projeto</span>
+            </v-card-title>
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field v-model="demand_create" label="Demanda*" required></v-text-field>
+                  </v-col>
+                  <v-col cols="30" sm="10" md="8">
+                    <v-text-field v-model="description_create" label="Descrição*" required></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="6">
+                    <v-text-field v-model="value_create" label="Orçamento*" required></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="6">
+                    <v-text-field v-model="hour_create" label="Horas Disponíveis*" required></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6">
+                    <v-select v-model="status_create" :items="this.status" label="Status*"></v-select>
+                  </v-col>
+                  <v-col cols="12" sm="6">
+                    <v-select v-model="user_create" :items="this.users" label="Responsável*"></v-select>
+                  </v-col>
+                </v-row>
+              </v-container>
+              <small>*indicates required field</small>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="red darken-1" text @click="dialog = false">Close</v-btn>
+              <v-btn color="orange darken-1" text @click="saveData">Save</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-col>
+      <v-col cols="1">
+        <v-dialog v-model="edit_dialog" persistent max-width="600px">
+          <template v-slot:activator="{ on }">
+            <v-btn color="orange" v-on="on">
+              <v-icon>mdi-pencil</v-icon>
+            </v-btn>
+          </template>
+          <v-card>
+            <v-card-title>
+              <span class="headline">Atualizar Projeto</span>
+            </v-card-title>
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col cols="12" sm="6">
+                    <v-select v-model="project_update" :items="this.projetos" label="Projetos*"></v-select>
+                  </v-col>
+                  <v-col cols="12" sm="6">
+                    <v-select v-model="status_update" :items="this.status" label="Status*"></v-select>
+                  </v-col>
+                </v-row>
+              </v-container>
+              <small>*indicates required field</small>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="red darken-1" text @click="edit_dialog = false">Close</v-btn>
+              <v-btn color="orange darken-1" text @click="updateData">Update</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-col>
+      <v-col cols="1">
+        <v-dialog v-model="delete_dialog" persistent max-width="600px">
+          <template v-slot:activator="{ on }">
+            <v-btn color="orange" v-on="on">
+              <v-icon>mdi-delete</v-icon>
+            </v-btn>
+          </template>
+          <v-card>
+            <v-card-title>
+              <span class="headline">Eliminar Projeto</span>
+            </v-card-title>
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col cols="12" sm="6">
+                    <v-select v-model="project_delete" :items="this.projetos" label="Projetos*"></v-select>
+                  </v-col>
+                </v-row>
+              </v-container>
+              <small>*indicates required field</small>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="red darken-1" text @click="delete_dialog = false">Close</v-btn>
+              <v-btn color="orange darken-1" text @click="deleteData">Delete</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
   
@@ -92,40 +130,59 @@ export default {
 
   methods: {
     deleteData() {
+      console.log(this.project_delete);
       axios
         .delete("http://127.0.0.1:8000/projects/delete/", {
-          project: this.project_field
+          project: this.project_delete
         })
         .then(response => {
           alert("Eliminado com sucesso!");
-          console.log(response)
-          this.dialog = false;
+          console.log(response);
           this.getData();
         })
         .catch(e => {
           console.error(e);
           alert("Erro ao Eliminar!");
         });
+
+      this.dialog = false;
+    },
+    updateData() {
+      axios
+        .put("http://127.0.0.1:8000/projects/update/", {
+          project: this.project_update,
+          status: this.status_update
+        })
+        .then(response => {
+          alert("Atualizado com sucesso!");
+          console.log(response);
+          this.getData();
+        })
+        .catch(e => {
+          console.error(e);
+          alert("Erro ao atualizar!");
+        });
+      this.dialog = false;
     },
     saveData() {
       axios
         .post("http://127.0.0.1:8000/projects/create/", {
-          demandCode: this.demand,
-          description: this.description,
-          user: this.user_field,
-          value: this.value,
-          hoursAp: this.hour,
-          status: this.status_field
+          demandCode: this.demand_create,
+          description: this.description_create,
+          user: this.user_create,
+          value: this.value_create,
+          hoursAp: this.hour_create,
+          status: this.status_create
         })
         .then(response => {
           alert("Inserido com sucesso!");
-          this.dialog = false;
           this.getData();
         })
         .catch(e => {
           console.error(e);
           alert("Erro ao Inserir!");
         });
+      this.dialog = false;
     },
     getData: function() {
       axios.get("http://127.0.0.1:8000/projects/user/").then(response => {
@@ -174,16 +231,20 @@ export default {
   data() {
     return {
       delete_dialog: false,
+      edit_dialog: false,
       dialog: false,
       search: "",
-      user_field: 0,
-      hour: 0,
-      status_field: "",
-      demand: 0,
-      description: "",
-      value: 0,
+      user_create: 0,
+      hour_create: 0,
+      status_create: "",
+      demand_create: 0,
+      description_create: "",
+      value_create: 0,
+      project_create: 0,
+      project_update: 0,
+      status_update: "",
+      project_delete: 0,
       users: [],
-      project_field: 0,
       projects: [],
       projetos: [],
       status: [
--- src/views/Value.vue
+++ src/views/Value.vue
@@ -1,42 +1,46 @@
 <template>
   <v-container fluid>
-    <v-row justify="left">
+    <v-row justify="center">
       <v-col cols="10">
         <material-card color="green" title="Gastos" text>
           <v-data-table :headers="headers" :items="values" :search="search" hide-default-footer />
         </material-card>
       </v-col>
     </v-row>
-    <v-dialog v-model="dialog" persistent max-width="600px">
-      <template v-slot:activator="{ on }">
-        <v-btn color="green" fab v-on="on">
-          <v-icon>mdi-plus</v-icon>
-        </v-btn>
-      </template>
-      <v-card>
-        <v-card-title>
-          <span class="headline">Apontamento de Gastos</span>
-        </v-card-title>
-        <v-card-text>
-          <v-container>
-            <v-row>
-              <v-col cols="12" sm="6" md="4">
-                <v-text-field v-model="value" label="Gastos *" required></v-text-field>
-              </v-col>
-              <v-col cols="12" sm="6">
-                <v-select v-model="project" :items="this.projects" label="Projeto *"></v-select>
-              </v-col>
-            </v-row>
-          </v-container>
-          <small>*indicates required field</small>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="red darken-1" text @click="dialog = false">Close</v-btn>
-          <v-btn color="green darken-1" text @click="saveData">Save</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+    <v-row justify="center">
+      <v-col cols="1">
+        <v-dialog v-model="dialog" persistent max-width="600px">
+          <template v-slot:activator="{ on }">
+            <v-btn color="green" fab v-on="on">
+              <v-icon>mdi-plus</v-icon>
+            </v-btn>
+          </template>
+          <v-card>
+            <v-card-title>
+              <span class="headline">Apontamento de Gastos</span>
+            </v-card-title>
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field v-model="value" label="Gastos *" required></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6">
+                    <v-select v-model="project" :items="this.projects" label="Projeto *"></v-select>
+                  </v-col>
+                </v-row>
+              </v-container>
+              <small>*indicates required field</small>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="red darken-1" text @click="dialog = false">Close</v-btn>
+              <v-btn color="green darken-1" text @click="saveData">Save</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -59,20 +63,20 @@ export default {
           project: this.project
         })
         .then(response => {
-          alert("Inserido com sucesso!")
-          this.dialog = false
+          alert("Inserido com sucesso!");
+          this.dialog = false;
           this.getData();
         })
         .catch(e => {
           console.error(e);
-          alert("Erro ao Inserir!")
+          alert("Erro ao Inserir!");
         });
     },
     getData: function() {
       axios.get("http://127.0.0.1:8000/values/").then(response => {
         this.values = response.data;
       });
-      axios.get("http://127.0.0.1:8000/projects/").then(response => {
+      axios.get("http://127.0.0.1:8000/projects/user/").then(response => {
         this.projects = response.data.map(item => {
           return { value: item.id, text: item.demandCode };
         });
